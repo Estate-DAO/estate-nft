6448d01ac3f1f4da38c38b2aea96bbbe28d6b8de
Harsh Es <harsh@estatedao.org>
2024-02-29 15:21:51 +0530

    Fix script

--- scripts/canisters_deploy_local.sh
+++ scripts/canisters_deploy_local.sh
@@ -10,14 +10,14 @@ gzip -f -1 ./target/wasm32-unknown-unknown/release/estate_dao_nft_backend.wasm
 minter_wasm="./target/wasm32-unknown-unknown/release/estate_dao_nft_backend.wasm.gz"
 
 # Use xxd to convert the file content to a hexadecimal string
-char=$(hexdump -ve '1/1 "%.2x"' "$minter_wasm")
+minter_wasm_char=$(hexdump -ve '1/1 "%.2x"' "$minter_wasm")
 
 # Escape special characters in the hexadecimal string
-char_escaped=$(printf "%s" "$char" | sed 's/../\\&/g')
+minter_wasm_char_escaped=$(printf "%s" "$minter_wasm_char" | sed 's/../\\&/g')
 
 cd ../provision_canister
 
-printf "( blob \"%s\")" "$char_escaped" > argument1
+printf "( blob \"%s\")" "$minter_wasm_char_escaped" > minter_wasm.blob
 
 dfx deploy provision_canister_backend
 dfx deploy internet_identity
@@ -26,16 +26,19 @@ dfx deploy internet_identity
 asset_wasm="./src/provision_canister_backend/assetstorage.wasm.gz"
 
 # Use xxd to convert the file content to a hexadecimal string
-asset_char=$(hexdump -ve '1/1 "%.2x"' "$asset_wasm")
+asset_wasm_char=$(hexdump -ve '1/1 "%.2x"' "$asset_wasm")
 
 # Escape special characters in the hexadecimal string
-asset_char_escaped=$(printf "%s" "$asset_char" | sed 's/../\\&/g')
+asset_wasm_char_escaped=$(printf "%s" "$asset_wasm_char" | sed 's/../\\&/g')
 
-printf "( blob \"%s\")" "$asset_char_escaped" > argument2
+printf "( blob \"%s\")" "$asset_wasm_char_escaped" > asset_wasm.blob
 
 
-dfx canister call provision_canister_backend init_minter_wasm --argument-file argument1
-dfx canister call provision_canister_backend init_asset_wasm --argument-file argument2
+dfx canister call provision_canister_backend init_minter_wasm --argument-file minter_wasm.blob
+dfx canister call provision_canister_backend init_asset_wasm --argument-file asset_wasm.blob
+
+rm mint_wasm.blob
+rm asset_wasm.blob
 
 can_id=$(jq -r '.provision_canister_backend.local' ./.dfx/local/canister_ids.json)
 dfx canister deposit-cycles 8000000000000 $can_id
